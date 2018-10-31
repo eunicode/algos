962eb79b3c8cbd2991ef2daea6da07f57d078ea9
Eunice Park <28845173+eunicode@users.noreply.github.com>
2018-10-30 23:54:22 -0700

    :recycle: Refactor by using post-incrementing

--- csx/3-closure-scope-excon/cycleIterator.js
+++ csx/3-closure-scope-excon/cycleIterator.js
@@ -17,6 +17,10 @@ After returning the last element of the array, the next invocation will return t
 ================================================================= */
 
 /*
+Version 1 - PRE-INCREMENT
+https://github.com/eunicode/algos/blob/7d5e0523da36573fc06d0f10ed75fcb6243000a7/csx/3-closure-scope-excon/cycleIterator.js
+
+Version 2 - POST-INCREMENT
 */
 
 /* =================================================================  
@@ -26,24 +30,20 @@ After returning the last element of the array, the next invocation will return t
 function cycleIterator(arr) {
     // We store count in the outer function, so it doesn't get reset after function inner runs.
     // This local scope is better than the global scope bc global variables result in collisions.
-    let count = 0;
+    // `count` is available to `inner` bc of lexical scoping.
+    // `count` survives bc of closure.
+    let index = 0;
 
     return function inner() {
-        count += 1; // ++count;
+        // POST-INCREMENT
+        // When post-incrementing, you can avoid index - 1, bc index is equal to 0.
+        const day = arr[index++]; // At this point, index = 0
 
-        // We use the remainder operator to cycle through the array.
-        let index = count % 3;
-
-        // If the count is a multiple of 3, the remainder will be 0. 
-        // 0 - 1 is negative one. 
-        // Therefore, if count is 3, or 6, etc, we want to return the third element in the array.
-        // aka, the element at index 2. 
-        if (index === 0) {
-            return arr[2];
+        if (index >= arr.length) { // Now, index = 1
+            index = 0;
         }
 
-        // We subtract one bc element 1 is at index 0.
-        return arr[index - 1];
+        return day;
     }
 }
 
@@ -73,8 +73,19 @@ console.log(getDay());
 ================================================================= */ 
 
 /* 
+REMAINDER OPERATOR
 When using the remainder operator, if the dividend is less than the divisor, the remainder operator will return the dividend. 
 Example: 4 % 8; // 4
+
+POST-INCREMENT VS PRE-INCREMENT
+index = index + 1;        
+const day = arr[index];   // has same effect as pre-incrementing
+    vs
+const day = arr[index++]; // post-increment
+
+const arr = ['a', 'b', 'c'];
+let index = 0;
+arr[index++]; // 'a'
 
 QUESTIONS
 Is `outer` stored inside the VariableEnvironment or execution context? 
